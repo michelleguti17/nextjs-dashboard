cd33630f3d8018f466688ad5cd19d9b24dcad73f
michellegutierrez <gut20014@byui.edu>
2024-03-13 22:01:05 -0500

    new changes

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -34,7 +34,7 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
-
+  noStore();
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
