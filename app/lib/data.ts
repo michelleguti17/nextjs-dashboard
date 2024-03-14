e6e82840e6ef0fee40c8dafaf14f2042e1e4258a
michellegutierrez <gut20014@byui.edu>
2024-03-13 22:33:40 -0500

    new changes

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,20 +12,16 @@ import { formatCurrency } from './utils';
 import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
-  // Add noStore() here to prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  noStore();
   try {
-    // Artificially delay a response for demo purposes.
+    // We artificially delay a response for demo purposes.
     // Don't do this in production :)
-
-     console.log('Fetching revenue data...');
+    console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
-
+ 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-     console.log('Data fetch completed after 3 seconds.');
-
+ 
+    console.log('Data fetch completed after 3 seconds.');
+ 
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
@@ -55,7 +51,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
- noStore()
+ noStore();
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
