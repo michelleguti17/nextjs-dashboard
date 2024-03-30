ec3b83c9fe52e6533e8d0a587c52b6b04f689128
michellegutierrez <gut20014@byui.edu>
2024-03-30 12:49:34 -0500

    validated

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -58,7 +58,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
   // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
     return {
@@ -66,7 +66,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       message: 'Missing Fields. Failed to Create Invoice.',
     };
   }
- 
+  console.log(validatedFields);
   // Prepare data for insertion into the database
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
