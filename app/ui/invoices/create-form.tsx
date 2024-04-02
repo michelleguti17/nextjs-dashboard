986c2a7136832c01f565096356b926c1d995a375
michelleguti17 <gut20014@byui.edu>
2024-04-01 21:53:57 -0500

    try meessage

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -129,7 +129,9 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </div>
           </div>
         </fieldset>
-
+        {state.message && (
+          <p className="mt-2 text-sm text-red-500">{state.message}</p>
+        )}
         {/* Action buttons */}
         <div className="mt-6 flex justify-end gap-4">
           <Link
