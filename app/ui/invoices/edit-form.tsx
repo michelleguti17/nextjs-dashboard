613439290dae95a53c49902a35fb3a86916bc52d
michelleguti17 <gut20014@byui.edu>
2024-04-01 21:56:30 -0500

    edit message added

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -143,6 +143,9 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </fieldset>
+        {state.message && (
+          <p className="mt-2 text-sm text-red-500">{state.message}</p>
+        )}
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
